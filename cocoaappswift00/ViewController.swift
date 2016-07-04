2c0667acf0c85b3ae17ff6f7dd081e8511b9ea74
KomoritaTsuyoshi <komoritatsuyoshi@komoritatsuyoshi-no-imac.local>
2016-07-04 16:46:40 +0900

    Initial Commit

---
+++ cocoaappswift00/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  cocoaappswift00
+//
+//  Created by KomoritaTsuyoshi on 2016/07/04.
+//  Copyright © 2016年 KomoritaTsuyoshi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
